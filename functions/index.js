e082dadc5a64db2e3927aa4784cba30292ff14cc
FullFireZoo <yassine60000@gmail.com>
2022-06-09 11:38:55 +0200

    sendMail

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,10 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const puppeteer = require("puppeteer");
 const { document } = require("firebase-functions/v1/firestore");
+const sendGridMail = require('@sendgrid/mail')
 admin.initializeApp();
-
+sendGridMail.setApiKey(`${process.env.KEY}`)
+    
 exports.screenShotElement = functions.https.onRequest(
   async (request, response) => {
     const browser = await puppeteer.launch();
@@ -20,25 +22,68 @@ exports.screenShotElement = functions.https.onRequest(
 exports.screenShot = functions.https.onRequest(async (request, response) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  if(request.body.html){
-    await page.setContent(request.body.html)
-  }else{await page.goto(request.body.url)}
- 
-  await page.evaluate(() => document.querySelectorAll("iframe").forEach((pub)=> {
+  if (request.body.html) {
+    await page.setContent(request.body.html);
+  } else {
+    await page.goto(request.body.url);
+  }
+  await page.evaluate(() =>
+    document.querySelectorAll("iframe").forEach((pub) => {
       pub.style.display = "none";
-  }));
+    })
+  );
   const imgBuffer = await page.screenshot({ fullPage: true });
   response.set("Content-Type", "image/png");
   response.status(200).send(imgBuffer);
 });
 
-exports.toPdf = functions.https.onRequest(async (request, response) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  if(request.body.html){
-    await page.setContent(request.body.html)
-  }else{await page.goto(request.body.url)}
-  const pdfBuffer = await page.pdf({ printBackground: true });
-  response.set("Content-Type", "application/pdf");
-  response.status(200).send(pdfBuffer);
-});
+const options = { memory: "1GB", timeoutSeconds: 30 };
+exports.toPdf = functions
+  .runWith(options)
+  .https.onRequest(async (request, response) => {
+    response.set("Access-Control-Allow-Origin", "*");
+    response.set("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS");
+    response.set("Access-Control-Allow-Headers", "*");
+    if (request.method === "OPTIONS") {
+      response.end();
+    }
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    if (request.body.html) {
+      await page.setContent(request.body.html);
+    } else {
+      await page.goto(request.body.url);
+    }
+    const pdfBuffer = await page.pdf({ printBackground: true });
+    response.set("Content-Type", "application/pdf");
+    response.status(200).send(pdfBuffer);
+  });
+ 
+ 
+
+  
+  
+exports.sendMail = functions.https.onRequest(
+    async (request, res) => {
+   
+    console.log(request.body.to);
+
+    const msg = {
+        to: request.body.to,
+        from : `${process.env.MAIL}`,
+        subject: request.body.subject,
+        html: request.body.html
+    }
+    console.log(msg);
+    sendGridMail
+      .send(msg)
+      .then((response) => {
+        res.send("mail envoyé")
+        console.log(response[0].statusCode)
+        console.log(response[0].headers)
+      })
+      .catch((error) => {
+        res.send("mail pas envoyé")
+        console.error(error)
+      })
+})
